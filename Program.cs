8ed2ad27214b1c7479bcf255db04feeab8d6be70
Yuikot <Adrianna.ryza1@wp.pl>
2024-02-13 21:50:36 +0100

    inital :3

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Threading;
+using InputSimulatorStandard;
+using InputSimulatorStandard.Native;
+
+class Program
+{
+
+    static void Main()
+    {
+        TimeSpan duration = TimeSpan.FromMilliseconds(1500);
+        DateTime startTime = DateTime.Now;
+        TimeSpan loopDuration = TimeSpan.FromMinutes(20);
+        int timer = 3;
+
+        Thread threadAD = new Thread(() =>
+        {
+            while (DateTime.Now - startTime < loopDuration)
+            {
+                PressAndHoldKey(ConsoleKey.A, duration);
+                PressAndHoldKey(ConsoleKey.D, duration);
+            }
+        });
+        threadAD.Start();
+
+        while (DateTime.Now - startTime < loopDuration)
+        {
+            PressKey(ConsoleKey.E, TimeSpan.FromMilliseconds(500));
+            if ((DateTime.Now - startTime).TotalMinutes % timer == 0)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+            }
+        }
+        threadAD.Join();
+    }
+
+    static void PressAndHoldKey(ConsoleKey key, TimeSpan duration)
+    {
+        var simulator = new InputSimulator();
+        simulator.Keyboard.KeyDown((VirtualKeyCode)key);
+        Thread.Sleep(duration);
+        simulator.Keyboard.KeyUp((VirtualKeyCode)key);
+    }
+
+    static void PressKey(ConsoleKey key, TimeSpan duration)
+    {
+        var simulator = new InputSimulator();
+        simulator.Keyboard.KeyPress((VirtualKeyCode)key);
+        Thread.Sleep(duration);
+    }
+}
